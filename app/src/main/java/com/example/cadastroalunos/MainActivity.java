4211beed06783521decd6748e2d15b0be60576d0
Lucas-Martins-de-Oliveira <lcmartins.oliveira@gmail.com>
2022-02-19 11:02:07 -0300

    criado intent MainAcitivty

--- app/src/main/java/com/example/cadastroalunos/MainActivity.java
+++ app/src/main/java/com/example/cadastroalunos/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.cadastroalunos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void cadastrarAluno(View view) {
-
+        Intent intent = new Intent(this, CadastroAlunoActivity.class);
+        startActivity(intent);
     }
 }
